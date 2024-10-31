28c5637329fe669ad61fbfe1f8698b27077efe9c
jakottelaar <jarno.kottelaar@gmail.com>
2024-10-31 22:50:10 +0100

    Created route handlers for folders
    POST and GET all

---
+++ src/app/api/folders/route.ts
@@ -0,0 +1,49 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest } from "next/server";
+import prisma from "../../../../prisma/client";
+import { z } from "zod";
+
+const folderSchema = z.object({
+  name: z.string(),
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    const { userId } = await auth();
+
+    const body = await req.json();
+
+    const validation = folderSchema.safeParse(body);
+
+    if (!validation.success) {
+      return Response.json({ error: validation.error.errors }, { status: 400 });
+    }
+
+    const folder = await prisma.folder.create({
+      data: {
+        name: validation.data.name,
+        userId: userId!,
+      },
+    });
+
+    return Response.json({ content: { folder: folder } }, { status: 201 });
+  } catch (error) {
+    return Response.json({ error: "Failed to create folder" }, { status: 500 });
+  }
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    const { userId } = await auth();
+
+    const folders = await prisma.folder.findMany({
+      where: {
+        userId: userId!,
+      },
+    });
+
+    return Response.json({ content: { folders: folders } });
+  } catch (error) {
+    return Response.json({ error: "Failed to fetch folders" }, { status: 500 });
+  }
+}
